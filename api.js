8061af1c9bbe9887ceb777f52ca5e9eae95c19d4
dwijgohil <20ecuot047@ddu.ac.in>
2023-07-16 23:50:41 +0530

    basic post api done.

--- api.js
+++ api.js
@@ -3,11 +3,19 @@ const dbConnect = require('./mongodb.js');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/',async (req,resp)=>{
     let db = await dbConnect();
     const data = await db.find().toArray();
 
     resp.send(data);
+});
+
+app.post('/',async(req,resp)=>{
+    let db = await dbConnect();
+    let result = await db.insertOne(req.body);
+    resp.send(result);
 });
 
 app.listen(5000);
